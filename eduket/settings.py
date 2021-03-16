4e69cc7f6fab3fe9af64fccf8f40feaac3ae3659
piyush0074 <piyushmakwana74@gmail.com>
2021-03-16 21:59:05 +0530

    #

--- eduket/settings.py
+++ eduket/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'veeuer*56vozl825(d)l10+bwek21viv#x^pqk1*4i@qgf)&5g'
+SECRET_KEY = 'Your secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eduket.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eduket.secret')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
